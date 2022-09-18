26baafe21c4043461046d5e03214688fd300b899
adesu7 <adityadesu100@gmail.com>
2022-09-18 15:15:59 +1000

    test dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
   ],
+  darkMode: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+      },
+    },
   },
   plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
 };
