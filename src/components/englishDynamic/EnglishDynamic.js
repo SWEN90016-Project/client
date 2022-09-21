722e0c4c04118515bf5a369328fe33df5030c9ab
mattwebbyy <mattwebb022@gmail.com>
2022-09-21 18:17:38 +1000

    test

--- src/components/englishDynamic/EnglishDynamic.js
+++ src/components/englishDynamic/EnglishDynamic.js
@@ -4,7 +4,7 @@ import Carousel from "./carousel";
 function EnglishDynamic() {
   return (
     <div className=" h-screen bg-blue-400" style={{ minHeight: 1080 }}>
-             <div className=" z-40  text-2xl sm:text-4xl md:text-4xl lg:text-6xl xl:text-9xl container mx-auto  py-12 sm:py-24 justify-center px-24"> Indigenous dynamic page This page is for aboriginal .... </div>
+             <div className=" z-40  text-2xl sm:text-4xl md:text-4xl lg:text-6xl xl:text-9xl container mx-auto  py-12 sm:py-24 justify-center px-24"> Indigenous dynamic page This page is for aboriginals .... </div>
 
       <div className=" h-1/2 justify-center align-middle grid grid-cols-3 ">
         <div className=" col-start-2 col-end-3">
