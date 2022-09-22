beff2701749e9ab6e2d37a3f7f43cfc24cd01976
swarnold123 <swarnold@student.unimelb.edu.au>
2022-09-22 13:37:12 +1000

    Stuff I have done

--- src/components/Fatsil.js
+++ src/components/Fatsil.js
@@ -4,6 +4,7 @@ function Fatsil() {
     <div className="h-screen bg-blue-400">
       <h1 className="text-black">Fatsil</h1>
       <Link to="/Homepage">Homepage</Link>
+      <p className="text-blue"> hey mate </p>
     </div>
   );
 }
