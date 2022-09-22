b6eb8b80dc4b4607396b74cbfd8e24b44442c45b
IAMLaunder <82994823+IAMLaunder@users.noreply.github.com>
2022-09-22 16:55:02 +1000

    account functionality working fully

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -44,18 +44,15 @@ export default function Nav() {
             </div>
           </div>
           <div className="ml-10 space-x-4">
-            <a
-              href="#"
+            <Link
+              to="/"
               className="inline-block rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75"
+              onClick={() => {
+                localStorage.removeItem("user");
+              }}
             >
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50"
-            >
-              Sign up
-            </a>
+              Log Out
+            </Link>
           </div>
         </div>
       </nav>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,93 @@
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
-
+import Form from "react-validation/build/Form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { useNavigate } from "react-router-dom";
+import { isEmail } from "validator";
+import AuthService from "../Services/auth.service.js";
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+const validEmail = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
+const vusername = (value) => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The username must be between 3 and 20 characters.
+      </div>
+    );
+  }
+};
+const vpassword = (value) => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The password must be between 6 and 40 characters.
+      </div>
+    );
+  }
+};
 function Welcome() {
+  let navigate = useNavigate();
+  const form = useRef();
+  const checkBtn = useRef();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [successful, setSuccessful] = useState(false);
+  const [message, setMessage] = useState("");
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+  const handleRegister = (e) => {
+    e.preventDefault();
+    setMessage("");
+    setSuccessful(false);
+    form.current.validateAll();
+    if (checkBtn.current.context._errors.length === 0) {
+      AuthService.register(username, email, password).then(
+        (response) => {
+          setMessage(response.data.message);
+          setSuccessful(true);
+          navigate("/Login");
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+          setMessage(resMessage);
+          setSuccessful(false);
+        }
+      );
+    }
+  };
   return (
     <div className="h-screen ">
       <div className="grid grid-cols-3">
@@ -33,7 +120,32 @@ function Welcome() {
             </div>
             <div className="w-full pt-4">
               <div className=" py-8 px-4 rounded-lg">
-                <form className="space-y-6" action="#" method="POST">
+                <Form
+                  className="space-y-6"
+                  onSubmit={handleRegister}
+                  ref={form}
+                >
+                  <div>
+                    <label
+                      htmlFor="username"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Username
+                    </label>
+                    <div className="mt-1">
+                      <Input
+                        type="text"
+                        name="username"
+                        id="username"
+                        required
+                        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 sm:text-sm"
+                        placeholder="Username"
+                        value={username}
+                        onChange={onChangeUsername}
+                        validations={[required, vusername]}
+                      />
+                    </div>
+                  </div>
                   <div>
                     <label
                       htmlFor="email"
@@ -42,52 +154,20 @@ function Welcome() {
                       Email
                     </label>
                     <div className="mt-1">
-                      <input
+                      <Input
                         type="email"
                         name="email"
                         id="email"
                         required
                         className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 sm:text-sm"
                         placeholder="you@example.com"
+                        value={email}
+                        onChange={onChangeEmail}
+                        validations={[required, validEmail]}
                       />
                     </div>
                   </div>
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      First name
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        required
-                        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 sm:text-sm"
-                        placeholder="you@example.com"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Last name
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        required
-                        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 sm:text-sm"
-                        placeholder="you@example.com"
-                      />
-                    </div>
-                  </div>
+
                   <div>
                     <label
                       htmlFor="password"
@@ -96,23 +176,42 @@ function Welcome() {
                       Password
                     </label>
                     <div className="mt-1">
-                      <input
+                      <Input
                         id="password"
                         name="password"
                         type="password"
                         autoComplete="current-password"
                         required
                         className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Password"
+                        value={password}
+                        onChange={onChangePassword}
+                        validations={[required, vpassword]}
                       />
                     </div>
                   </div>
+                  {message && (
+                    <div className="form-group">
+                      <div
+                        className={
+                          successful
+                            ? "alert alert-success"
+                            : "alert alert-danger"
+                        }
+                        role="alert"
+                      >
+                        {message}
+                      </div>
+                    </div>
+                  )}
                   <button
                     type="submit"
                     className="bg-blue-500 px-8 py-2 rounded-md text-white"
                   >
                     Register
                   </button>
-                </form>
+                  <CheckButton style={{ display: "none" }} ref={checkBtn} />
+                </Form>
               </div>
             </div>
           </div>
