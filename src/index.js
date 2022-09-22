37a8ca5bf91d89d599da60fc299f07a9a9e851b0
IAMLaunder <82994823+IAMLaunder@users.noreply.github.com>
2022-09-22 15:33:58 +1000

    updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./index.css";
 import App from "./App.js";
 import "flowbite";
+import "tw-elements";
 
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("app");
