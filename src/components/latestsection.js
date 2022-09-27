b1540aef21cf89197d0f5891943ebce289a00397
adesu7 <adityadesu100@gmail.com>
2022-09-27 12:02:39 +1000

    more images

--- src/components/latestsection.js
+++ src/components/latestsection.js
@@ -1,8 +1,72 @@
 import blogSection from "./englishDynamic/blogSection";
-import AborginalFlag from "./assets/aboriginal.png";
+import AboriginalFlag from "./assets/aboriginal.png";
 import AboriginalHuman from "./assets/aboriginalCool.jpg";
 import TorresStraitFlag from "./assets/torres.png";
 import AboriginalArt from "./assets/aboriginalArt.jpeg";
+import BlueAboriginalArt from "./assets/aboriginal-art-symbols-star.gif";
+
+const posts = [
+  {
+    title: "Title of database pulled post",
+    href: "#",
+    category: { name: "Article", href: "#" },
+    description: "This is a short abstract of post ",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    imageUrl: AboriginalFlag,
+    readingTime: "6 min",
+    author: {
+      name: "Name of author",
+      href: "#",
+      imageUrl: AboriginalFlag,
+    },
+  },
+  {
+    title: "Title of database pulled post",
+    href: "#",
+    category: { name: "Video", href: "#" },
+    description: "This is a short abstract of post",
+    date: "Mar 10, 2020",
+    datetime: "2020-03-10",
+    imageUrl: TorresStraitFlag,
+    readingTime: "4 min",
+    author: {
+      name: "Name of author",
+      href: "#",
+      imageUrl: TorresStraitFlag,
+    },
+  },
+  {
+    title: "Title of database pulled post",
+    href: "#",
+    category: { name: "Case Study", href: "#" },
+    description: "This is a short abstract of post",
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: AboriginalHuman,
+    readingTime: "11 min",
+    author: {
+      name: "Name of author",
+      href: "#",
+      imageUrl: AboriginalHuman,
+    },
+  },
+  {
+    title: "Title of database pulled post",
+    href: "#",
+    category: { name: "Case Study", href: "#" },
+    description: "This is a short abstract of post",
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: AboriginalArt,
+    readingTime: "11 min",
+    author: {
+      name: "Name of author",
+      href: "#",
+      imageUrl: AboriginalArt,
+    },
+  },
+];
 
 export default function lastestblogs() {
   return (
@@ -16,7 +80,7 @@ export default function lastestblogs() {
           <div class="lg:flex">
             <img
               class="object-cover w-full h-56 rounded-lg lg:w-64"
-              src={TorresStraitFlag}
+              src={BlueAboriginalArt}
               alt=""
             />
 
@@ -39,7 +103,7 @@ export default function lastestblogs() {
           <div class="lg:flex">
             <img
               class="object-cover w-full h-56 rounded-lg lg:w-64"
-              src={AborginalFlag}
+              src={AboriginalFlag}
               alt=""
             />
 
@@ -108,7 +172,7 @@ export default function lastestblogs() {
           <div class="lg:flex">
             <img
               class="object-cover w-full h-56 rounded-lg lg:w-64"
-              src={AborginalFlag}
+              src={AboriginalFlag}
               alt=""
             />
 
