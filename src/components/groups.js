dd08f99b6521148f2835fc095c7a9eb4e8dcf476
IAMLaunder <82994823+IAMLaunder@users.noreply.github.com>
2022-10-03 14:23:15 +1100

    groups page structure 70% done

--- src/components/groups.js
+++ src/components/groups.js
@@ -1,19 +1,39 @@
 import axios from "axios";
 import { useEffect, useState, Fragment, useRef } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Transition, Combobox } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
 // import { CheckIcon } from "@heroicons/react/outline";
 const API_URL = "http://localhost:9000/api/";
 
 function Groups() {
   const [allGroups, setAllGroups] = useState("");
   const [userGroups, setUserGroups] = useState("");
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [users, setUsers] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [groupName, setgroupName] = useState("");
+  const [selected, setSelected] = useState("select user");
+  const [query, setQuery] = useState("");
+  const onChangeGroupName = (e) => {
+    const text = e.target.value;
+    setgroupName(text);
+  };
+  const filteredUsers =
+    query === ""
+      ? users
+      : users.filter((person) =>
+          person.username
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   const cancelButtonRef = useRef(null);
   const getAllGroups = async () => {
     try {
       const response = await axios.get(API_URL + "getGroup");
-      setAllGroups(response.data);
+      setAllGroups(response.data.data);
       console.log(allGroups);
     } catch (error) {
       console.error(error);
@@ -30,6 +50,7 @@ function Groups() {
   const addGroup = async (groupName) => {
     try {
       axios.post(API_URL + "addGroup", { groupName: groupName });
+      window.location.reload();
     } catch (error) {
       console.error(error);
     }
@@ -46,63 +67,78 @@ function Groups() {
       console.error(error);
     }
   };
+  const getAllUser = async () => {
+    try {
+      const response = await axios.get(API_URL + "userInfo");
+      setUsers(response.data.data);
+
+      setLoading(false);
+      // setSelected(users[0].username);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
     getAllGroups();
     console.log(allGroups);
+    getAllUser();
+
+    // console.log(users[0].username);
   }, []);
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading</h1>
+      </div>
+    );
+  }
   return (
-    <div className="">
-      <p>test</p>
-      <button
-        onClick={() => {
-          addGroup("testingput");
-        }}
-      >
-        butoon for testing
-      </button>
-      <button
-        onClick={() => {
-          addUser();
-        }}
-      >
-        second butoon for testing
-      </button>
+    <div className="py-2">
       <div className="grid grid-flow-row">
         <div className="grid grid-cols-6">
           <h1 className="">Groups</h1>
-          <div className="col-end-7 col-span-2 flex flex-row">
+          <div className="col-end-7 col-span-3 flex flex-row gap-2">
             <input
               id="text"
               name="text"
               type="text"
               placeholder="Text"
+              onChange={onChangeGroupName}
               className=" block appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             />
-            <button className="w-12">new group</button>
-          </div>
-        </div>
-        <div className="grid grid-flow-row bg-white rounded-lg">
-          <div className="grid grid-flow-col">
-            <p>GroupName</p>
             <button
               onClick={() => {
-                setOpen(true);
+                addGroup(groupName);
               }}
-              className="place-self-end"
+              className="bg-green-400 p-2 rounded-lg"
             >
-              add User
+              new group
             </button>
           </div>
+        </div>
+        <div className="grid grid-flow-row gap-2 p-2 ">
+          {/* filteredUsers.map((user) => ( */}
+          {allGroups.map((group) => (
+            <div key={group._id} className="p-2 bg-white rounded-lg">
+              <div className="grid grid-flow-col">
+                <p>{group.groupName}</p>
+                <button
+                  onClick={() => {
+                    setOpen(true);
+                  }}
+                  className="place-self-end bg-blue-400 rounded-lg p-2"
+                >
+                  add User
+                </button>
+              </div>
 
-          <div className="grid grid-cols-5 grid-flow-row">
-            <p>Username</p>
-            <p>Username</p>
-            <p>Username</p>
-            <p>Username</p>
-            <p>Username</p>
-            <p>Username</p>
-            <p>Username</p>
-          </div>
+              <div className="grid grid-cols-5 grid-flow-row">
+                {group.userList.map((user) => (
+                  <p key={group._id + user.username}>{user.username}</p>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <Transition.Root show={open} as={Fragment}>
@@ -135,28 +171,95 @@ function Groups() {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <Dialog.Panel className="relative transform overflow-autp rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                   <div>
-                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                      {/* <CheckIcon
-                        className="h-6 w-6 text-green-600"
-                        aria-hidden="true"
-                      /> */}
-                    </div>
                     <div className="mt-3 text-center sm:mt-5">
                       <Dialog.Title
                         as="h3"
                         className="text-lg font-medium leading-6 text-gray-900"
                       >
-                        Payment successful
+                        Add New User
                       </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Lorem ipsum, dolor sit amet consectetur adipisicing
-                          elit. Eius aliquam laudantium explicabo pariatur iste
-                          dolorem animi vitae error totam. At sapiente aliquam
-                          accusamus facere veritatis.
-                        </p>
+                      <div className="flex items-center justify-center">
+                        <div className=" w-72">
+                          <Combobox value={selected} onChange={setSelected}>
+                            <div className="relative mt-1">
+                              <div className="relative w-full cursor-default overflow-visible rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                                <Combobox.Input
+                                  className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                                  displayValue={(users) => users.username}
+                                  onChange={(event) =>
+                                    setQuery(event.target.value)
+                                  }
+                                />
+                                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                                  <ChevronUpDownIcon
+                                    className="h-5 w-5 text-gray-400"
+                                    aria-hidden="true"
+                                  />
+                                </Combobox.Button>
+                              </div>
+                              <Transition
+                                as={Fragment}
+                                leave="transition ease-in duration-100"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                                afterLeave={() => setQuery("")}
+                              >
+                                <Combobox.Options className="absolute z-20 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                  {filteredUsers.length === 0 &&
+                                  query !== "" ? (
+                                    <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                                      Nothing found.
+                                    </div>
+                                  ) : (
+                                    filteredUsers.map((user) => (
+                                      <Combobox.Option
+                                        key={user._id}
+                                        className={({ active }) =>
+                                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                            active
+                                              ? "bg-teal-600 text-white"
+                                              : "text-gray-900"
+                                          }`
+                                        }
+                                        value={user}
+                                      >
+                                        {({ selected, active }) => (
+                                          <>
+                                            <span
+                                              className={`block truncate ${
+                                                selected
+                                                  ? "font-medium"
+                                                  : "font-normal"
+                                              }`}
+                                            >
+                                              {user.username}
+                                            </span>
+                                            {selected ? (
+                                              <span
+                                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                  active
+                                                    ? "text-white"
+                                                    : "text-teal-600"
+                                                }`}
+                                              >
+                                                <CheckIcon
+                                                  className="h-5 w-5"
+                                                  aria-hidden="true"
+                                                />
+                                              </span>
+                                            ) : null}
+                                          </>
+                                        )}
+                                      </Combobox.Option>
+                                    ))
+                                  )}
+                                </Combobox.Options>
+                              </Transition>
+                            </div>
+                          </Combobox>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -166,7 +269,7 @@ function Groups() {
                       className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
                       onClick={() => setOpen(false)}
                     >
-                      Deactivate
+                      Add User
                     </button>
                     <button
                       type="button"
