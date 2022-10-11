ea85bb714142a0f9c9f72e796aad982f46d29a91
adesu7 <adityadesu100@gmail.com>
2022-10-11 12:14:36 +1100

    Fatsil copy right link

--- src/components/Fatsil.js
+++ src/components/Fatsil.js
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 function Fatsil() {
   return (
-    <div className="h-screen bg-blue-400">
-      <h1 className="text-black">Fatsil</h1>
-      <Link to="/Homepage">Homepage</Link>
-      <p className="text-blue"> hey mate </p>
+    <div className="container px-6 py-10 mx-auto">
+      <div class="flex flex-col justify-between py-6 lg:mx-6">
+        <p className="text-sm font-medium text-white-600">
+          <a
+            href="https://www.wipo.int/export/sites/www/tk/en/databases/creative_heritage/docs/fatsil_protocol_guide.pdf"
+            className="hover:underline"
+          >
+            Fatsil Protocol Guide
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
